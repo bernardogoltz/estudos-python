c067a61aa2f8154d3c1a555f028c6d1616212c39
bernardogoltz <78585520+bernardogoltz@users.noreply.github.com>
2022-03-12 15:05:56 -0300

    conta
    a

--- orientacao-obj/conta.py
+++ orientacao-obj/conta.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+
 """
 Created on Sat Mar 12 14:14:39 2022
 
@@ -7,5 +8,22 @@ Created on Sat Mar 12 14:14:39 2022
 """
 
 class Conta: 
-    pass
+    
+    # __function__ -> função construtora. 
+    
+    def __init__(self , numero , titular , saldo , limite):
+        print("Construindo objeto: {}".format(self))
+        self.numero = numero
+        self.titular = titular
+        self.saldo = saldo
+        self.limite = limite
+    
+    def extrato(self):
+        print("{} -> R$ {}".format(self.saldo , self.titular))
+        
+    def depositar(self , valor):
+        self.saldo += valor
+    
+    def sacar(self , valor):
+        self.saldo -= valor
 
