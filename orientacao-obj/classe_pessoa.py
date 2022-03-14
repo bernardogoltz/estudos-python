66503de6cfbb10a2b8f7cd46792092f0b31a6018
bernardogoltz <78585520+bernardogoltz@users.noreply.github.com>
2022-03-14 09:17:55 -0300

    classe pessoa - getters e setters
    aprendendo getters e setters e @property

---
+++ orientacao-obj/classe_pessoa.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 14 08:48:37 2022
+@title: pessoa orientada a objetos. 
+@author: bernardogoltz
+"""
+
+class Pessoa: 
+    # notação d classe em letra maíuscula 
+    
+    def __init__(self, nome, idade): 
+        self.__nome = nome
+        self.__idade = idade
+        
+    # "__init__" função construtora da classe, "__" define um método especial
+    # o parâmetro self é uma convenção e refere-se ao próprio objeto. 
+    
+    @property
+    def nome(self): 
+        print("executando @property nome")
+        return self.__nome.title()
+    
+        # getter -> método get, retorna um atributo de um objeto. 
+        # método title trata o nome e devolve em letra maíuscula. 
+        
+    def nome(self , nome):
+        self.__nome = nome 
+        
+        
