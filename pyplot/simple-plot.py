e4ad0c4c6fd81b10e5ac25bf2b13d49dde9b5827
Bernardo <bergoltzx2@gmail.com>
2022-06-02 09:12:11 -0300

    sin plot

--- pyplot/simple-plot.py
+++ pyplot/simple-plot.py
@@ -2,6 +2,26 @@
 """
 Created on Thu Jun  2 09:00:33 2022
 
-@author: angel
+@author: bernardogoltz
 """
 
+# Simple plot example. 
+
+import matplotlib.pyplot as plt 
+import numpy as np
+
+plt.style.use('_mpl-gallery')
+
+x = np.linspace(0,np.pi*2,50)
+y = np.sin(x)
+
+# subplots()
+
+fig, ax = plt.subplots(figsize = (6,3))
+
+ax.set_title("Seno de x")
+
+ax.set_xlabel("Valores para o eixo X")
+ax.set_ylabel("Valores para o eixo Y")
+
+ax.plot(x,y,linewidth = 2.5)
