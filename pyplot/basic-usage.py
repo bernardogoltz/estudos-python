dbeaa24cc3ba673202346b67bd928630d059416e
Bernardo <bergoltzx2@gmail.com>
2022-06-02 09:01:40 -0300

    turning back to study plt

---
+++ pyplot/basic-usage.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun  2 08:35:33 2022
+
+@author: angel
+"""
+
---
+++ pyplot/simple-plot.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun  2 09:00:33 2022
+
+@author: angel
+"""
+
